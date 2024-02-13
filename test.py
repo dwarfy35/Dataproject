db08ffa1cc2e8cb75bbabc9f2808e3fec5ab94f4
kandevander <hanshenrik2300@gmail.com>
2024-02-13 11:53:30 +0100

    changed in the test.py file

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+path = 'ctDNA_frag_meth/data/kmer_tsv/2mers_extend200/'
+
+df1_unmeth = pd.read_csv(path + 'sample1_unmethylated.tsv', sep='\t')
+
+df1_meth = pd.read_csv(path + 'sample1_methylated.tsv', sep='\t')
+
+bgdf1_unmeth= pd.read_csv(path + 'background_unmethylated.tsv', sep='\t')
+
+bgdf1_meth = pd.read_csv(path + 'background_methylated.tsv', sep='\t')
+
+print(df1_unmeth.head())
