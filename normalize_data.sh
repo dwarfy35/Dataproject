5c469ced923ffd71cfe4fb4868ddd68d156a7f16
kandevander <hanshenrik2300@gmail.com>
2024-02-20 19:44:50 +0100

    Changed the file so it can be used for the split files

--- normalize_data.sh
+++ normalize_data.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-mkdir -p data_normalized
+mkdir -p data_normalized_split
 
-cd ctDNA_frag_meth/data/kmer_tsv/2mers_extend200/
+cd ctDNA_frag_meth_split/data/kmer_tsv/2mers_extend200_split/
 
 for file in *.tsv
 do
     sum=$(awk -F'\t' '{sum+=$2} END {print sum}' $file)
-    awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized/2mers_extend200/${file%.tsv}_normalized.tsv
+    awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized_split/2mers_extend200/${file%.tsv}_normalized.tsv
 done
 
-cd ../4mers_extend200/
+cd ../4mers_extend200_split/
 
 for file in *.tsv
 do
     sum=$(awk -F'\t' '{sum+=$2} END {print sum}' $file)
-    awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized/4mers_extend200/${file%.tsv}_normalized.tsv
+    awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized_split/4mers_extend200/${file%.tsv}_normalized.tsv
 done
 
-cd ../6mers_extend200/
+cd ../6mers_extend200_split/
 
 for file in *.tsv
 do
     sum=$(awk -F'\t' '{sum+=$2} END {print sum}' $file)
-    awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized/6mers_extend200/${file%.tsv}_normalized.tsv
+    awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized_split/6mers_extend200/${file%.tsv}_normalized.tsv
 done
