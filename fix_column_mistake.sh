28f0e817c7f668aed158103698861b6c2d6776eb
kandevander <hanshenrik2300@gmail.com>
2024-02-20 14:38:40 +0100

    Refactored some of the usage of 'normalize_data.sh' into its own file since I keep on making the same mistake

---
+++ fix_column_mistake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $1
+
+
+# it then goes through all tsv files and replaces the first line with 'kmer' + a tab + 'count'
+
+for file in *.tsv
+do
+    sed -i '1s/.*/kmer\tcount/' $file
+done
+echo "Done"
--- normalize_data.sh
+++ normalize_data.sh
@@ -25,21 +25,3 @@ do
     sum=$(awk -F'\t' '{sum+=$2} END {print sum}' $file)
     awk -v sum=$sum -F'\t' '{print $1"\t"$2/sum}' $file > ../../../../data_normalized/6mers_extend200/${file%.tsv}_normalized.tsv
 done
-
-cd ../../../../data_normalized/2mers_extend200/
-for file in *.tsv
-do
-    sed -i '1s/^/kmer\tcount\n/' $file
-done
-
-cd ../4mers_extend200/
-for file in *.tsv
-do
-    sed -i '1s/^/kmer\tcount\n/' $file
-done
-
-cd ../6mers_extend200/
-for file in *.tsv
-do
-    sed -i '1s/^/kmer\tcount\n/' $file
-done
